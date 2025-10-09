09c3ba337b9d8ba05d2d69359dce3344747d7839
VictorRodas99 <victormatiasrodas100@gmail.com>
2025-10-09 17:02:27 -0300

    setup google fonts + setup toast providers + setup tanstack query providers

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,12 +1,46 @@
+import { ToastContainer } from '@/resources/components/toast'
+import { ToastProvider } from '@/resources/context/toast.context'
 import '@/resources/css/global.css'
+import {
+  Inter_400Regular,
+  Inter_500Medium,
+  Inter_600SemiBold,
+  Inter_700Bold,
+  useFonts
+} from '@expo-google-fonts/inter'
 import { PortalHost } from '@rn-primitives/portal'
-import { Stack } from 'expo-router'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { Slot, SplashScreen } from 'expo-router'
+import { useEffect } from 'react'
+
+SplashScreen.preventAutoHideAsync()
+const queryClient = new QueryClient()
 
 export default function RootLayout() {
+  const [loaded, error] = useFonts({
+    Inter_400Regular,
+    Inter_500Medium,
+    Inter_600SemiBold,
+    Inter_700Bold
+  })
+
+  useEffect(() => {
+    if (loaded || error) {
+      SplashScreen.hideAsync()
+    }
+  }, [loaded, error])
+
+  if (!loaded && !error) {
+    return null
+  }
+
   return (
-    <>
-      <Stack />
-      <PortalHost />
-    </>
+    <QueryClientProvider client={queryClient}>
+      <ToastProvider>
+        <ToastContainer />
+        <Slot />
+        <PortalHost />
+      </ToastProvider>
+    </QueryClientProvider>
   )
 }
--- app/index.tsx
+++ app/index.tsx
@@ -3,8 +3,11 @@ import { Text, View } from 'react-native'
 export default function Index() {
   return (
     <View className="flex-1 items-center justify-center bg-white">
-      <Text className="text-xl font-bold text-blue-500">
-        Welcome to Nativewind!
+      <Text
+        style={{ fontFamily: 'Inter-Black' }}
+        className="text-xl font-bold text-blue-500"
+      >
+        Holaaa Mundo!
       </Text>
     </View>
   )
