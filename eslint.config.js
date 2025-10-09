72590eb8bf111cca97db8e446637f4ea1d24f7ac
VictorRodas99 <victormatiasrodas100@gmail.com>
2025-10-09 11:37:39 -0300

    configure eslint to work with prettier

--- eslint.config.js
+++ eslint.config.js
@@ -1,10 +1,27 @@
 // https://docs.expo.dev/guides/using-eslint/
-const { defineConfig } = require('eslint/config');
-const expoConfig = require('eslint-config-expo/flat');
+const { defineConfig } = require('eslint/config')
+const expoConfig = require('eslint-config-expo/flat')
+const prettierConfig = require('eslint-config-prettier')
+const prettierPlugin = require('eslint-plugin-prettier')
 
 module.exports = defineConfig([
-  expoConfig,
+  ...expoConfig,
+  prettierConfig,
   {
-    ignores: ['dist/*'],
+    plugins: {
+      prettier: prettierPlugin
+    },
+    rules: {
+      'prettier/prettier': [
+        'error',
+        {},
+        {
+          usePrettierrc: true
+        }
+      ]
+    }
   },
-]);
+  {
+    ignores: ['dist/*']
+  }
+])
