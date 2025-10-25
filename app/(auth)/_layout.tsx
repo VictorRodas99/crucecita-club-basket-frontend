2e23f9f88178cc9ce26f4967fa1bc0e32a3f5b88
VictorRodas99 <victormatiasrodas100@gmail.com>
2025-10-25 15:33:04 -0300

    add safe area view + add light visuals + adapt styles

--- app/(auth)/_layout.tsx
+++ app/(auth)/_layout.tsx
@@ -1,5 +1,8 @@
+import LightBackground from '@/resources/components/light-background'
 import { useSession } from '@/resources/hooks/session'
 import { Redirect, Slot } from 'expo-router'
+import { View } from 'react-native'
+import { SafeAreaView } from 'react-native-safe-area-context'
 
 export default function AuthLayout() {
   const { session } = useSession()
@@ -8,5 +11,12 @@ export default function AuthLayout() {
     return <Redirect href="/" />
   }
 
-  return <Slot />
+  return (
+    <View className="flex-1 bg-primary/80 dark:bg-secondary">
+      <SafeAreaView className="flex-1">
+        <LightBackground />
+        <Slot />
+      </SafeAreaView>
+    </View>
+  )
 }
