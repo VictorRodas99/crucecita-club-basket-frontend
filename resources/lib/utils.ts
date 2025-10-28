f3eed5eb778f7b0142223e3e4639bccafe5e3d0b
VictorRodas99 <victormatiasrodas100@gmail.com>
2025-10-28 18:52:35 -0300

    add capitalize function

--- resources/lib/utils.ts
+++ resources/lib/utils.ts
@@ -4,3 +4,11 @@ import { twMerge } from 'tailwind-merge'
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+export function capitalize(string: string) {
+  if (typeof string !== 'string') {
+    throw new Error('Expected a string')
+  }
+
+  return string[0].toUpperCase() + string.substring(1)
+}
