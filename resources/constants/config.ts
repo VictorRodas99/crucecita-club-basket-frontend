cf2a33d35ff1ac4ba4c8099dd64a3a1e09066717
VictorRodas99 <victormatiasrodas100@gmail.com>
2025-10-28 18:51:26 -0300

    add user roles as constants

--- resources/constants/config.ts
+++ resources/constants/config.ts
@@ -2,3 +2,10 @@ export const APP_URL = 'http://localhost:8000'
 export const API_PREFIX = '/api'
 
 export const API_URL: string = APP_URL + API_PREFIX
+
+export const USER_ROLE = {
+  admin: 'administrador',
+  coach: 'entrenador',
+  tutor: 'tutor',
+  delegate: 'delegado'
+} as const
