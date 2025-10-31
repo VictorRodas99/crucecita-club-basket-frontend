2e56063f708eb5210d0a0b52c813397239077763
VictorRodas99 <victormatiasrodas100@gmail.com>
2025-10-31 16:26:53 -0300

    add date parser

--- resources/lib/utils.ts
+++ resources/lib/utils.ts
@@ -12,3 +12,18 @@ export function capitalize(string: string) {
 
   return string[0].toUpperCase() + string.substring(1)
 }
+
+/**
+ * @param dateString DD/MM/YYYY
+ */
+export const parseDate = (dateString: string): Date | null => {
+  if (!dateString || dateString.length !== 10) return null
+
+  const [day, month, year] = dateString.split('/').map(Number)
+  if (!day || !month || !year) return null
+
+  const date = new Date(year, month - 1, day)
+  if (isNaN(date.getTime())) return null
+
+  return date
+}
