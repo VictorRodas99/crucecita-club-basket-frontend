e5a88c88bbb467f1495c1e4893747cd7200797dc
VictorRodas99 <victormatiasrodas100@gmail.com>
2025-10-31 16:27:53 -0300

    extract common input styles

--- resources/components/primitives/input.tsx
+++ resources/components/primitives/input.tsx
@@ -1,6 +1,9 @@
 import { cn } from '@/resources/lib/utils'
 import { Platform, TextInput, type TextInputProps } from 'react-native'
 
+export const DEFAULT_INPUT_STYLES =
+  'dark:bg-input/30 border-input bg-background text-foreground flex h-10 w-full min-w-0 flex-row items-center rounded-md border px-3 py-1 text-base leading-5 shadow-sm shadow-black/5 sm:h-9'
+
 function Input({
   className,
   ...props
@@ -8,7 +11,7 @@ function Input({
   return (
     <TextInput
       className={cn(
-        'dark:bg-input/30 border-input bg-background text-foreground flex h-10 w-full min-w-0 flex-row items-center rounded-md border px-3 py-1 text-base leading-5 shadow-sm shadow-black/5 sm:h-9',
+        DEFAULT_INPUT_STYLES,
         props.editable === false &&
           cn(
             'opacity-50',
