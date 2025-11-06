3f31609ce2353e0db370eb2c53e532843ef6b030
VictorRodas99 <victormatiasrodas100@gmail.com>
2025-11-06 14:42:39 -0300

    fix date parsing to handle months superior to 12 + add the method to calculate the exact years since given date

--- resources/lib/utils.ts
+++ resources/lib/utils.ts
@@ -20,10 +20,36 @@ export const parseDate = (dateString: string): Date | null => {
   if (!dateString || dateString.length !== 10) return null
 
   const [day, month, year] = dateString.split('/').map(Number)
-  if (!day || !month || !year) return null
+
+  if (!day || !month || !year) {
+    return null
+  }
+
+  if (month > 12) {
+    return null
+  }
 
   const date = new Date(year, month - 1, day)
   if (isNaN(date.getTime())) return null
 
   return date
+}
+
+export const getYearsSince = (date: Date) => {
+  if (!(date instanceof Date)) {
+    throw new Error('Invalid date')
+  }
+
+  const today = new Date()
+
+  today.setHours(0, 0, 0)
+  date.setHours(0, 0, 0)
+
+  let years = today.getFullYear() - date.getFullYear()
+
+  if (today.getMonth() < date.getMonth()) {
+    years--
+  }
+
+  return years
 }
