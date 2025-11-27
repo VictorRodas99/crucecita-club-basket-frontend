02125577ed0b7baa993ef435bdf6b67aae66c70e
VictorRodas99 <victormatiasrodas100@gmail.com>
2025-11-27 17:39:03 -0300

    add tanstack query mutations for login, register and logout actions + wrap ky requests + handle correctly errors

---
+++ resources/features/auth/use-auth-mutations.ts
@@ -0,0 +1,40 @@
+import { User } from '@/resources/context/session.context'
+import { toFormData } from '@/resources/lib/formatters'
+import http from '@/resources/lib/http'
+import { AuthApiResponse } from '@/resources/types/api-responses'
+import { LoginForm, RegisterFormData } from '@/resources/types/forms/auth'
+import { useMutation } from '@tanstack/react-query'
+
+export function useLoginMutation() {
+  return useMutation({
+    mutationFn: async (data: LoginForm) =>
+      http
+        .post('login', { body: await toFormData(data) })
+        .json<AuthApiResponse<User>>()
+  })
+}
+
+export function useRegisterMutation() {
+  return useMutation({
+    mutationFn: async (data: RegisterFormData) =>
+      http
+        .post('register', { body: await toFormData(data) })
+        .json<AuthApiResponse<User>>()
+  })
+}
+
+export async function logout({ token }: { token?: string }) {
+  try {
+    await http
+      .post('logout', {
+        headers: { Authorization: `Bearer ${token}` }
+      })
+      .json()
+
+    return true
+  } catch (error) {
+    console.error(error)
+
+    return false
+  }
+}
