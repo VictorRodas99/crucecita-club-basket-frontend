1c85bafbcad634b1be66af3a697e5ad88c7d056d
VictorRodas99 <victormatiasrodas100@gmail.com>
2025-11-27 17:40:08 -0300

    change script logic to use sonner's toast + refactor code

--- resources/lib/handle-api-errors.ts
+++ resources/lib/handle-api-errors.ts
@@ -2,19 +2,23 @@
 // https://www.luckymedia.dev/about/lokman-musliu
 
 import type { FieldValues, Path, UseFormSetError } from 'react-hook-form'
-import { toast as primititveToast } from 'sonner-native'
+import { toast } from 'sonner-native'
+import { ErrorResponse } from '../types/api-responses'
 
 export async function handleApiErrors<T extends FieldValues>({
   error,
-  setError,
-  toast
+  setError
 }: {
   error: any
   setError: UseFormSetError<T>
-  toast: typeof primititveToast
 }): Promise<void> {
-  if (error?.name === 'HTTPError' && error.response.status === 422) {
-    const errors = await (error as any).response.json()
+  if (error?.name === 'HTTPError' && error.response?.status === 422) {
+    const response = (await error.response.json()) as ErrorResponse
+    const errors = response?.errors
+
+    if (!errors) {
+      return
+    }
 
     Object.keys(errors).forEach((field) => {
       // Join error messages on one single string
@@ -28,7 +32,7 @@ export async function handleApiErrors<T extends FieldValues>({
       })
     })
   } else {
-    // console.log(JSON.stringify(error, null, 2));
+    console.error({ error })
     toast.error('Algo salió mal')
   }
 }
