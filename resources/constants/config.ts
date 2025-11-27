4ace333b5f6796b3f9acaf879626b3031c2c7462
VictorRodas99 <victormatiasrodas100@gmail.com>
2025-11-27 17:35:49 -0300

    change app url constant to be exact ip, this fixes a Network Issue with react native and the docker network

--- resources/constants/config.ts
+++ resources/constants/config.ts
@@ -1,4 +1,5 @@
-export const APP_URL = 'http://localhost:8000'
+export const APP_URL = 'http://192.168.1.103:8000'
+
 export const API_PREFIX = '/api'
 
 export const API_URL: string = APP_URL + API_PREFIX
